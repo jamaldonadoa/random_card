882ddbd774c0c8d3c20c39d6acf72a15541b3774
jamaldonadoa <jadolfomaldonadoa@gmail.com>
2021-10-17 21:51:40 +0000

    Carta terminada

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pinta = ["♦", "♥", "♠", "♣"];
+let letra = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+function randomValor(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let htmlColorPinta = "";
+let htmlColorNum = "";
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numSelect = letra[randomValor(0, 12)];
+  let pintaSelec = pinta[randomValor(0, 3)];
+
+  if (pintaSelec == "♥" || pintaSelec == "♦") {
+    document.querySelector(".pintasup").id = "rojo";
+    document.querySelector(".pintainf").id = "rojo";
+    document.querySelector(".nro").id = "rojo";
+  }
+
+  document.querySelector(".pintasup").innerHTML = pintaSelec;
+  document.querySelector(".pintainf").innerHTML = pintaSelec;
+  document.querySelector(".nro").innerHTML = numSelect;
 };
